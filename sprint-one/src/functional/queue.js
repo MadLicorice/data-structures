5213307e7e331f1340b7d43ba4b4afbe26e82a6c
MadLicorice <madlicorice@gmail.com>
2018-01-17 15:22:52 -0800

    Finish functional-class stack, attempt queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var len = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    len += 1;
+    someInstance[Number(len - 1)] = value;
   };
 
   someInstance.dequeue = function() {
+    debugger;
+    if (len !== 0) {
+      len -= 1;
+    }
+    var popValue = someInstance[0]
+    delete someInstance[0];
+    return popValue;
   };
 
   someInstance.size = function() {
+    return len;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,29 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+  // var count = (Object.keys(storage)).length;
 
   // Implement the methods below
+  
   someInstance.push = function(value) {
+    // debugger;
+    count += 1;
+    return storage[Number(count - 1)] = value;
   };
 
   someInstance.pop = function() {
+    if (count !== 0) {
+      count -= 1;
+    }
+    var popValue = storage[(Number(count))];
+    delete storage[(Number(count))];
+    return popValue;
+
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
