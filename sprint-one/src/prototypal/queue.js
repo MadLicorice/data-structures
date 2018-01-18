0a467c870d5b91da58f192d77e750c38921b21c4
MadLicorice <madlicorice@gmail.com>
2018-01-18 00:04:19 -0800

    Finish stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.len = 0;
+  someInstance.first = 0;
+  someInstance.last = 0;
+
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.last] = value;
+  this.last += 1;
+  this.len += 1;
+};
+
+queueMethods.dequeue = function() {
+  var deqValue;
+  if (this.len !== 0) {
+    deqValue = this.storage[this.first];
+    delete this.storage[this.first];
+    this.len -= 1;
+    this.first += 1;
+  }
+  return deqValue;
+};
+
+queueMethods.size = function () {
+  return this.len;
+};
+
+
+
+
+
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.count = 0;
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[Number(this.count)] = value;
+  this.count += 1;
+};
+
+stackMethods.pop = function() {
+  var popValue = this.storage[Number((this.count - 1))];
+  if (this.count !== 0) {
+    delete this.storage[Number((this.count - 1))];
+    this.count -= 1;
+  }
+  return popValue;
+
+};
+
+stackMethods.size = function () {
+  return this.count;
+};
 
