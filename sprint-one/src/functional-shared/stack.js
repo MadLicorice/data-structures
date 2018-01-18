50a22512a3cf59f3c66254b7278223cd5a011638
MadLicorice <madlicorice@gmail.com>
2018-01-17 21:39:52 -0800

    Finish stack but constructor passing test fails

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,43 @@
+// var extend = function(to, from) {
+//   for (var key in from) {
+//     if (key !== constructor) {
+//       to[key] = from[key];
+//     }
+//   }
+// };
+
+
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+
+  someInstance.count = 0;
+
+  _.extend(someInstance, Stack.stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
 
 
+Stack.stackMethods = {};
+
+Stack.stackMethods.push = function(value) {
+  this.storage[Number(this.count)] = value;
+  this.count += 1;
+};
+
+Stack.stackMethods.pop = function() {
+  var popValue = this.storage[Number((this.count - 1))];
+  if (this.count !== 0) {
+    delete this.storage[Number((this.count - 1))];
+    this.count -= 1;
+  }
+  return popValue;
+
+};
+
+Stack.stackMethods.size = function () {
+  return this.count;
+};
