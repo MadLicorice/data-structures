2565bab236dc669fe79653368112a1d5a3542a41
MadLicorice <madlicorice@gmail.com>
2018-01-18 07:45:08 -0800

    Complete es6 stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,28 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.len = 0;
+    this.first = 0;
+    this.last = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.last] = value;
+    this.last += 1;
+    this.len += 1;
+  }
+
+  dequeue() {
+    if (this.len !== 0) {
+      var deqValue = this.storage[this.first];
+      delete this.storage[this.first];
+      this.first += 1;
+      this.len -= 1;
+      return deqValue;
+    }
+  }
+
+  size() {
+    return this.len;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,25 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  push(value) {
+    this.storage[this.count] = value;
+    this.count += 1;
+  }
+
+  pop() {
+    if (this.count !== 0) {
+      var popValue = this.storage[this.count - 1];
+      delete this.storage[this.count - 1];
+      this.count -= 1;
+      return popValue;
+    }
+  }
+
+  size() {
+    return this.count;
+  }
 }
+
