3c0b6fb4b6078fb04008aa6f1fee150c82f2e6e8
MadLicorice <madlicorice@gmail.com>
2018-01-20 14:33:39 -0800

    Finish hashTable.js just before solution lecture!

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,42 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k, v];
+  var arr = this._storage.get(index);
+
+  if (arr) {
+    arr.push(tuple);
+    this._storage.set(index, arr);
+  } else {
+    var access = this._storage.set(index, []);
+    var arr = this._storage.get(index);
+    arr.push(tuple);
+    this._storage.set(index, arr);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var access = this._storage.get(index);
+  var value;
+  access.forEach(function(elem) {
+    if (elem[0] === k) {
+      value = elem[1];
+    }
+  });
+  return value;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var access = this._storage.get(index);
+  access.forEach(function(elem) {
+    if (elem[0] === k) {
+      access.splice(elem, 1);
+    }
+  });
 };
 
 
