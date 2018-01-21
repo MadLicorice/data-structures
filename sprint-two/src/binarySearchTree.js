3cbf0cb65b5c6a3b8c8bd1e92bb31814686b3f65
MadLicorice <madlicorice@gmail.com>
2018-01-21 13:58:53 -0800

    Finish BST.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,86 @@
 var BinarySearchTree = function(value) {
+  var BST = {};
+
+  BST.value = value;
+  BST.right = null;
+  BST.left = null;
+  
+
+  BST.insert = function(value) {
+
+    if (value > BST.value) {
+      if (BST.right === null) {
+        var newNode = BinarySearchTree(value);
+        BST.right = newNode;
+      } else {
+        BST.right.insert(value);
+      }
+    } else {
+      if (BST.left === null) {
+        var newNode = BinarySearchTree(value);
+        BST.left = newNode;
+      } else {
+        BST.left.insert(value);
+      }
+    }
+
+    return newNode;
+  };
+
+  BST.contains = function(value) {
+    if (value === BST.value) {
+      return true;
+    }
+
+    if (value > BST.value) {
+      if (BST.right === null) {
+        return false;
+      } else {
+        if (BST.right.contains(value)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      if (BST.left === null) {
+        return false;
+      } else {
+        if (BST.left.contains(value)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  };
+  
+  BST.depthFirstLog = function(cb) {
+    debugger;
+    cb(BST.value);
+    var left = !!BST.left;
+    var right = !!BST.right;
+
+    if (left && right) {
+      BST.left.depthFirstLog(cb);
+      BST.right.depthFirstLog(cb);
+    } else if (left) {
+      BST.left.depthFirstLog(cb);
+    } else if (right) {
+      BST.right.depthFirstLog(cb);
+    } 
+
+  };
+
+  return BST;
 };
 
+// if value === target
+//   return true;
+// else 
+//   link.head = link.head.next;
+//   link.head.contains(target);
+//   return true;
 
 /*
  * Complexity: What is the time complexity of the above functions?
